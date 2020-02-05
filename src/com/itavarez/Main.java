0109eed24e7e02ce42d4cf517d9fed9da8a1ea2f
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-02-05 14:41:23 -0500

    Finished practicing with Arrays

--- src/com/itavarez/Main.java
+++ src/com/itavarez/Main.java
@@ -1,5 +1,7 @@
 package com.itavarez;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,15 +10,23 @@ public class Main {
         //fixed length
         //fixed index
         System.out.println("*** Arrays ***");
-//        Dogs dogArr[];
-//        dogArr = new Dogs[5]; // 0- 4 index
+//        Dogs dogArr[]; // Old way of creating an Array
+//        dogArr = new Dogs[5]; // specifying the length => 0- 4 index
         Dogs[] dogArr = new Dogs[5];
         dogArr[0] = new Dogs("springer", 50, false);
+        dogArr[1] = new Dogs("Bulldog", 50, true);
+        dogArr[2] = new Dogs("collie", 50, false);
+        dogArr[3] = new Dogs("Chihuahua", 5, false);
         dogArr[4] = new Dogs("Corgie", 35, true);
+
         for(int i = 0; i < dogArr.length; i++) {
             System.out.println(dogArr[i]);
         }
 
+        System.out.println(" Breed 3 good for apartment?" + dogArr[3].isApartment());
+        System.out.println("\nprint out arrays");
+        System.out.println(dogArr.toString()); // prints out the pointer in memory
+        System.out.println(Arrays.toString(dogArr)); // Quick way of seeing the array
         //fixed element type
         // varied length
         //fixed index
