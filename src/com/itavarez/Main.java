c533070e3742ec60c6a2df5318be147186c09a25
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-02-05 14:19:43 -0500

    Create an Array using the class Dogs

--- src/com/itavarez/Main.java
+++ src/com/itavarez/Main.java
@@ -3,6 +3,28 @@ package com.itavarez;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+	    //fixed element type
+        //fixed length
+        //fixed index
+        System.out.println("*** Arrays ***");
+//        Dogs dogArr[];
+//        dogArr = new Dogs[5]; // 0- 4 index
+        Dogs[] dogArr = new Dogs[5];
+        dogArr[0] = new Dogs("springer", 50, false);
+        dogArr[4] = new Dogs("Corgie", 35, true);
+        for(int i = 0; i < dogArr.length; i++) {
+            System.out.println(dogArr[i]);
+        }
+
+        //fixed element type
+        // varied length
+        //fixed index
+        System.out.println("*** ArrayList ***");
+
+        //choice in index
+        //fixed element type
+        //varied length
+        System.out.println("*** HashMaps ***");
     }
 }
