d92ebf8ad5aa5feaf050745068e6fd08691fb48e
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2020-02-05 17:10:42 -0500

    create a HashMap and Sorted out Alphabetically

--- src/com/itavarez/Main.java
+++ src/com/itavarez/Main.java
@@ -1,7 +1,6 @@
 package com.itavarez;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 
@@ -20,8 +19,9 @@ public class Main {
         dogArr[3] = new Dogs("Chihuahua", 5, false);
         dogArr[4] = new Dogs("Corgie", 35, true);
 
-        for(int i = 0; i < dogArr.length; i++) {
-            System.out.println(dogArr[i]);
+        for (int i = 0; i < dogArr.length; i++) {
+            Dogs value = dogArr[i];
+            System.out.println(value);
         }
 
         System.out.println(" Breed 3 good for apartment?" + dogArr[3].isApartment());
@@ -34,9 +34,8 @@ public class Main {
         System.out.println("*** ArrayList ***");
          //Object -> Collections -> List -> ArrayList
         //created array list
-        ArrayList<Dogs> dogsArrayList = new ArrayList<Dogs>();
         //I'm converting the dogArr into ArrayList
-        dogsArrayList.addAll(Arrays.asList(dogArr));
+        ArrayList<Dogs> dogsArrayList = new ArrayList<Dogs>(Arrays.asList(dogArr));
 
         dogsArrayList.add(new Dogs("Mutt", 15, true));
 
@@ -82,6 +81,48 @@ public class Main {
         //choice in index
         //fixed element type
         //varied length
+        //Keys -> reference (index) unique
+        //values -> find using the key
         System.out.println("*** HashMaps ***");
+        //Created a HashMap with a key of Integer, and values of Dogs
+        HashMap<Integer, Dogs> dogsHashMap = new HashMap<Integer, Dogs>();
+
+        int hashCount = 0;
+        for(Dogs d: dogsArrayList) {
+            // put = if the key does not exist, add it to the hashMap;
+            //if key does exist - replace it
+            dogsHashMap.put(hashCount, d);
+            hashCount++;
+        }
+        //Removed the Array list from memory
+        dogsArrayList.clear();
+        System.out.println(dogsHashMap.get(3));
+        System.out.println(dogsHashMap.size());
+        dogsHashMap.remove(3);
+        System.out.println(dogsHashMap.get(3));
+
+        for (Integer i : dogsHashMap.keySet()) {
+            System.out.println("Key: " + i + " value: " + dogsHashMap.get(i));
+        }
+
+        //Sort HasMap
+        ArrayList<HashMap.Entry<Integer, Dogs>> sortedMap = new ArrayList<HashMap.Entry<Integer, Dogs>>();
+        sortedMap.addAll(dogsHashMap.entrySet());
+
+        //I'm sorting the HashMap Alphabetically order
+        Collections.sort(sortedMap, new Comparator<HashMap.Entry<Integer, Dogs>>() {
+            @Override
+            public int compare(HashMap.Entry<Integer, Dogs> o1, HashMap.Entry<Integer, Dogs> o2) {
+                return o1.getValue().getBreed().compareToIgnoreCase(o2.getValue().getBreed());
+            }
+        });
+
+        for (HashMap.Entry<Integer, Dogs> d : sortedMap) {
+            System.out.println("Key: " + d.getKey() + " value: " +  d.getValue());
+        }
     }
 }
+
+
+
+
